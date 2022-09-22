7ca4b3cab874c08c77f5fd4e7897d07d8eb03b75
Dan <106495422+elliottdanielp@users.noreply.github.com>
2022-09-22 12:44:25 -0400

    Update MechaCarChallenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -27,14 +27,14 @@ lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarise(
   Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), 
   .groups = 'keep')
 
-# t-test for all the lots, pop mu = 1500 psi
-t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+# T-test for all manufacturing lots
+t.test(Suspension_Coil$PSI, mu = 1500)
 
-# t-test for lot 1
-t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
+# Lot 1
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
 
-# t-test for lot 2
-t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
+# Lot 2
+t.test(subset(Suspension_Coil,Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
 
-# t-test for lot 3
-t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))
+# Lot 3
+t.test(subset(Suspension_Coil,Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
